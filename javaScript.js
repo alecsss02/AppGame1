bf6eb97521b97b1c10bd9debc1ec83f7abefaea8
alecsss02 <alexandrumoscalu@gmail.com>
2015-05-11 15:51:38 +0200

    Application
    Application for App project

---
+++ javaScript.js
@@ -0,0 +1,4 @@
+document.getElementById("startDate").innerHTML = "20.04.2015";
+document.getElementById("totalTime").innerHTML = "15 hours 45 minutes";
+document.getElementById("frequency").innerHTML = "daily";
+document.getElementById("totalScore").innerHTML = "150";
